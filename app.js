6f5304eef2b13130de34af1aee5790cd82219251
JaislinBajwa <89400877+JaislinBajwa@users.noreply.github.com>
2023-08-02 14:58:12 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ButtonWithEffect from './ButtonWithEffect';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonWithEffect />
+    </div>
+  );
+}
+
+export default App;
